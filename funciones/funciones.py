3c1db3b532d503bf561650a1da8e305683958261
RedondoDEV <javierRedondoDEV@gmail.com>
2024-10-28 16:12:47 +0100

    Commit to restore

--- funciones/funciones.py
+++ funciones/funciones.py
@@ -1,6 +1,7 @@
 from random import randint
 from clases.Futbolista import Futbolista
 
+
 def seleccionar_jugador_oculto(jugadores):
     # Selecciona un jugador oculto (es el que hay que adivinar)
     indice_random = randint(0, len(jugadores) - 1)
@@ -8,6 +9,7 @@ def seleccionar_jugador_oculto(jugadores):
     return Futbolista(jugador_oculto['Dorsal'], jugador_oculto['Edad'], jugador_oculto['Equipo'],
                       jugador_oculto['Nacionalidad'], jugador_oculto['Nombre'], jugador_oculto['Pie'],
                       jugador_oculto['Posicion'])
+
 
 def introduce_futbolista(jugadores):
     # Solicita al usuario que introduzca el nombre de un futbolista.
@@ -34,7 +36,7 @@ def buscar_futbolista(jugadores, nombre):
 def comprobar_victoria(jugador_intento, jugador_oculto):
     # Comprueba si el intento del usuario coincide con el jugador oculto.
     if jugador_intento.nombre == jugador_oculto.nombre:
-        print("Ganaste")
+        print(f"Ganaste, era {jugador_oculto.nombre}.")
         return True
     else:
         print("Inténtalo de nuevo")
